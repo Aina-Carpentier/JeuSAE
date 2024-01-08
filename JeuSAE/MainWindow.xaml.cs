9101c4b8e7a79f8e5a970788feb3d0db5c669815
Aina-Carpentier <aina.carpentier@etu.univ-smb.fr>
2024-01-08 17:05:55 +0100

    Debut creation gameengine

--- JeuSAE/MainWindow.xaml.cs
+++ JeuSAE/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace JeuSAE
 {
@@ -20,10 +21,63 @@ namespace JeuSAE
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        private int countTick = 0;
+        private bool gauche = false, droite = false, haut = false, bas = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            dispatcherTimer.Tick += GameEngine;
+            // rafraissement toutes les 16 milliseconds
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(16);
+            // lancement du timer
+            dispatcherTimer.Start();
+        }
 
+        private void GameEngine(object sender, EventArgs e)
+        {
+            Rect player = new Rect(Canvas.GetLeft(rectJoueur), Canvas.GetTop(rectJoueur), rectJoueur.Width, rectJoueur.Height);
+            if (gauche)
+            {
+                Canvas.SetLeft(rectCarte, Canvas.GetLeft(rectCarte) + 100);
+            }
+            if (droite)
+            {
+                Canvas.SetLeft(rectCarte, Canvas.GetLeft(rectCarte) - 100);
+            }
+            if (haut)
+            {
+                Canvas.SetTop(rectCarte, Canvas.GetLeft(rectCarte) + 100);
+            }
+            if (bas)
+            {
+                Canvas.SetTop(rectCarte, Canvas.GetLeft(rectCarte) - 100);
+            }
+
+        }
+        private void CanvasKeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+                gauche = true;
+            if (e.Key == Key.Right)
+                droite = true;
+            if (e.Key == Key.Up)
+                haut = true;
+            if (e.Key == Key.Down)
+                bas = true;
+        }
+
+        private void CanvasKeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+                gauche = false;
+            if (e.Key == Key.Right)
+                droite = false;
+            if (e.Key == Key.Up)
+                haut = false;
+            if (e.Key == Key.Down)
+                bas = false;
         }
     }
 }
