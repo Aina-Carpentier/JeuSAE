dfced5bc58c0f3462f4229d25b099f7769a57be8
Aiko <gerardelliot49@gmail.com>
2024-01-09 17:20:42 +0100

    Code optimization

--- JeuSAE/MapGenerator.cs
+++ JeuSAE/MapGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -13,16 +14,28 @@ namespace JeuSAE
     internal class MapGenerator
     {
         private const string COIN_HAUT_DROIT_REGEX = "grass_top_right_border_[0-9]+";
+        private const string COIN_DROIT_REGEX = "grass_right_border_[0-9]+";
+        private const string COIN_BAS_DROIT_REGEX = "grass_bottom_right_border_[0-9]+";
+        private const string COIN_HAUT_GAUCHE_REGEX = "grass_top_left_border_[0-9]+";
+        private const string COIN_GAUCHE_REGEX = "grass_left_border_[0-9]+";
+        private const string COIN_BAS_GAUCHE_REGEX = "grass_bottom_left_border_[0-9]+";
+
+        private readonly static List<String> REGEX = new List<string> { COIN_BAS_DROIT_REGEX, COIN_BAS_GAUCHE_REGEX, COIN_DROIT_REGEX, COIN_GAUCHE_REGEX, COIN_HAUT_DROIT_REGEX, COIN_HAUT_GAUCHE_REGEX };
+
         private readonly static List<ImageBrush> COIN_HAUT_DROITE_IMAGES = new List<ImageBrush> { };
-        private readonly static List<Uri> CHEMIN_IMAGES = new List<Uri> { };
+        private readonly static List<ImageBrush> COIN_HAUT_GAUCHE_IMAGES = new List<ImageBrush> { };
+        private readonly static List<ImageBrush> COIN_BAS_DROITE_IMAGES = new List<ImageBrush> { };
+        private readonly static List<ImageBrush> COIN_BAS_GAUCHE_IMAGES = new List<ImageBrush> { };
+        private readonly static List<ImageBrush> COIN_DROITE_IMAGES = new List<ImageBrush> { };
+        private readonly static List<ImageBrush> COIN_GAUCHE_IMAGES = new List<ImageBrush> { };
 
         public static void load()
         {
-            ChargerCheminsImages();
-            ChargerImagesCoinDroit();
+            ChargerImages();
+
         }
 
-        private static void ChargerCheminsImages()
+        private static void ChargerImages()
         {
             string[] tableau = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "images\\");
 
@@ -30,25 +43,34 @@ namespace JeuSAE
             {
                 if (File.Exists(file))
                 {
-                    CHEMIN_IMAGES.Add(new Uri(file));
+                    ChargerImage(new Uri(file));
                 }
             }
         }
 
-        private static void ChargerImagesCoinDroit()
+        private static void ChargerImage(Uri uri)
         {
-            foreach (Uri uri in CHEMIN_IMAGES)
+            String nomFichier = Path.GetFileName(uri.LocalPath);
+            foreach (String regex in REGEX)
             {
-                String nomFichier = System.IO.Path.GetFileName(uri.LocalPath);
-
-                if (Regex.IsMatch(nomFichier, COIN_HAUT_DROIT_REGEX))
+                if (Regex.IsMatch(nomFichier, regex))
                 {
                     ImageBrush image = new ImageBrush();
                     image.ImageSource = new BitmapImage(uri);
-                    COIN_HAUT_DROITE_IMAGES.Add(image);
+                    // listOuLaCharger.Add(image);
+
+                    switch (regex)
+                    {
+                        case COIN_HAUT_DROIT_REGEX: COIN_HAUT_DROITE_IMAGES.Add(image); break;
+                        case COIN_HAUT_GAUCHE_REGEX: COIN_HAUT_GAUCHE_IMAGES.Add(image); break;
+                        case COIN_BAS_DROIT_REGEX: COIN_BAS_DROITE_IMAGES.Add(image); break;
+                        case COIN_BAS_GAUCHE_REGEX: COIN_BAS_GAUCHE_IMAGES.Add(image); break;
+                        case COIN_DROIT_REGEX: COIN_DROITE_IMAGES.Add(image); break;
+                        case COIN_GAUCHE_REGEX: COIN_GAUCHE_IMAGES.Add(image); break;
+                    }
 
 #if DEBUG
-                    Console.WriteLine("Chargement du coin droit " + nomFichier + " terminé" + COIN_HAUT_DROITE_IMAGES[COIN_HAUT_DROITE_IMAGES.Count - 1]);
+                    Console.WriteLine("Chargement de l'image " + nomFichier + " terminé");
 #endif
                 }
             }
